d5df240f02cf8493b35f5247a11e90acbfaef095
swordcube <swordsoftarchive@gmail.com>
2024-01-08 04:33:44 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# hxdr_mp3
+
+Haxe/hxcpp @:native bindings for [DR MP3](https://github.com/mackron/dr_libs/blob/master/dr_mp3.h).
+
+---
+
+This library only works with the Haxe `cpp` target via `hxcpp`.
+
+---
+
+## Installation
+
+Install through the haxelib
+
+```
+haxelib install hxdr_mp3
+```
+
+or with git for the latest potentially unstable updates.
+
+```
+haxelib git hxdr_mp3 https://github.com/swordcube/hxdr_mp3.git
+```
+
+### Licensing
+`hxdr_mp3` is made available via the **MIT** license. Check [LICENSE](https://github.com/swordcube/hxdr_mp3/blob/main/LICENSE) for more information.
---
+++ src/dr/MP3.hx
@@ -0,0 +1,45 @@
+package dr;
+
+
+// WARNING: This will not have every feature dr_mp3 has to offer!
+//
+// Make an issue explaining what feature you want implemented over on GitHub!
+// https://github.com/swordcube/hxdr_mp3/issues
+
+@:include("vendor/include/dr_mp3.h")
+@:buildXml("<include name=\"${haxelib:hxdr_mp3}/include.xml\"/>")
+extern class DrMP3 {
+    @:native('drmp3_open_file_and_read_pcm_frames_s16')
+    static inline function openFileAndReadPCMFramesShort16(fileName:cpp.ConstCharStar, config:cpp.Pointer<DrMP3Config>, totalFrameCount:DrMP3UInt64, allocationCallbacks:cpp.ConstPointer<DrMP3AllocationCallbacks>):cpp.Star<cpp.Int16> {
+		return untyped __cpp__("drmp3_open_file_and_read_pcm_frames_s16({0}, {1}, {2}, {3})", fileName, config, cpp.Pointer.addressOf(totalFrameCount), allocationCallbacks);
+	}
+    
+    static inline function free(data:Any, allocationCallbacks:cpp.ConstPointer<DrMP3AllocationCallbacks>):Void {
+        untyped __cpp__('drmp3_free((void*){0}, {1})', data, allocationCallbacks);
+    }
+}
+
+@:include("vendor/include/dr_mp3.h")
+@:buildXml("<include name=\"${haxelib:hxdr_mp3}/include.xml\"/>")
+@:native('drmp3_allocation_callbacks')
+@:structAccess
+extern class DrMP3AllocationCallbacks {
+	var userData:Any;
+
+	var onMalloc:Any;
+	var onRealloc:Any;
+	var onFree:Any;
+}
+
+@:include("vendor/include/dr_mp3.h")
+@:buildXml("<include name=\"${haxelib:hxdr_mp3}/include.xml\"/>")
+@:native('drmp3_config')
+@:structAccess
+extern class DrMP3Config {
+    var channels:cpp.UInt32;
+    var sampleRate:cpp.UInt32;
+}
+
+@:native("long long unsigned int")
+@:scalar @:coreType @:notNull
+extern abstract DrMP3UInt64 from Int to Int {}
