9c7add9e7ea90db066cb499cc55b3ecf90fa7bc8
swordcube <swordsoftarchive@gmail.com>
2024-01-29 15:12:59 -0600

    Rename DrMP3 class to MP3

--- src/dr/MP3.hx
+++ src/dr/MP3.hx
@@ -8,7 +8,7 @@ package dr;
 
 @:include("vendor/include/dr_mp3.h")
 @:buildXml("<include name=\"${haxelib:hxdr_mp3}/include.xml\"/>")
-extern class DrMP3 {
+extern class MP3 {
     @:native('drmp3_open_file_and_read_pcm_frames_s16')
     static inline function openFileAndReadPCMFramesShort16(fileName:cpp.ConstCharStar, config:cpp.Pointer<DrMP3Config>, totalFrameCount:DrMP3UInt64, allocationCallbacks:cpp.ConstPointer<DrMP3AllocationCallbacks>):cpp.Star<cpp.Int16> {
 		return untyped __cpp__("drmp3_open_file_and_read_pcm_frames_s16({0}, {1}, {2}, {3})", fileName, config, cpp.Pointer.addressOf(totalFrameCount), allocationCallbacks);
